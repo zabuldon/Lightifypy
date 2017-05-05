f6bdd6693ece6e1159a8d6726929f71685e41e5b
Sergey Isachenko <vortexius@gmail.com>
2017-05-05 13:08:09 +0300

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='Lightifypy',
-    version='0.0.3',
+    version='0.0.4',
     packages=['lightifypy'],
     include_package_data=True,
     license='WTFPL',
